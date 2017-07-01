3986ab5778db1af47ab35f1ac9b59b58ae6ff450
Oliver Wright <meiamsome@meiamso.me>
2017-07-02 00:16:27 +0100

    Multithread loading, fix loading bugs

--- pollution/pollution_app/management/commands/openaq.py
+++ pollution/pollution_app/management/commands/openaq.py
@@ -1,3 +1,4 @@
+from concurrent import futures
 from django.core.management.base import BaseCommand, CommandError
 from django.utils import timezone
 from pollution_app.models import Country, City, Location, Measurement
@@ -9,21 +10,21 @@ api = API()
 ENTRIES_PER_PAGE = 1000
 
 
-def update_countries():
+def update_countries(ex):
     for country in api.countries(page=1, limit=ENTRIES_PER_PAGE)['results']:
-
         obj, _ = Country.objects.get_or_create(
             code=country['code'],
             defaults={
                 'name': country['name']
             }
         )
-        print("{}".format(obj.name))
-        update_cities(obj)
-        print(obj.name)
+    return "Countries", [
+        ex.submit(update_cities, ex, country)
+        for country in Country.objects.all()
+    ]
 
 
-def update_cities(country):
+def update_cities(ex, country):
     page = floor((1 + country.cities.count()) / ENTRIES_PER_PAGE) + 1
     stop = False
     while not stop:
@@ -35,13 +36,14 @@ def update_cities(country):
                 name=city['city'],
                 country=country,
             )
-            print("-{}\x1b[0K".format(obj.name))
-            update_locations(obj)
         page += 1
-    print("\r\x1b[0K\r\x1b[1A", end="")
+    return "Country {}".format(country.name), [
+        ex.submit(update_locations, ex, city)
+        for city in City.objects.filter(country=country)
+    ]
 
 
-def update_locations(city):
+def update_locations(ex, city):
     page = floor((1 + city.locations.count()) / ENTRIES_PER_PAGE) + 1
     stop = False
     while not stop:
@@ -61,44 +63,52 @@ def update_locations(city):
                     'city': city
                 }
             )
-
-            print("|-{}\x1b[0K".format(obj.name))
-            update_measurements(obj)
         page += 1
-    print("\r\x1b[0K\r\x1b[1A", end="")
+    return "City {}".format(city.name), [
+        ex.submit(update_measurements, ex, location)
+        for location in Location.objects.filter(city=city)
+    ]
 
 
-def update_measurements(location):
+def update_measurements(ex, location):
     page = floor((1 + location.measurements.count()) / ENTRIES_PER_PAGE) + 1
-    stop = False
-    while not stop:
-        result = api.measurements(page=page, limit=ENTRIES_PER_PAGE,
-                                  location=location.name, sort="asc",
-                                  order_by="date")
-        stop = page * result['meta']['limit'] > result['meta']['found']
-        for measures in result['results']:
-            date = datetime.strptime(measures['date']['utc'],
-                                     '%Y-%m-%dT%H:%M:%S.%fZ')
-            date = date.replace(tzinfo=timezone.utc)
+    result = api.measurements(page=page, limit=ENTRIES_PER_PAGE,
+                              location=location.name, sort="asc",
+                              order_by="date")
+    stop = page * result['meta']['limit'] > result['meta']['found']
+    for measures in result['results']:
+        date = datetime.strptime(measures['date']['utc'],
+                                 '%Y-%m-%dT%H:%M:%S.%fZ')
+        date = date.replace(tzinfo=timezone.utc)
 
-            Measurement.objects.get_or_create(
-                location=location,
-                utc=date,
-                parameter=measures['parameter'],
-                defaults={
-                    'parameter': measures['parameter'],
-                    'value': measures['value'],
-                    'unit': measures['unit'],
-                    'utc': date,
-                    'location': location
-                }
-            )
+        Measurement.objects.get_or_create(
+            location=location,
+            utc=date,
+            parameter=measures['parameter'],
+            defaults={
+                'parameter': measures['parameter'],
+                'value': measures['value'],
+                'unit': measures['unit'],
+                'utc': date,
+                'location': location
+            }
+        )
 
-            print("\r||-{}\x1b[0K".format(measures['date']['utc']), end="")
-        page += 1
-    print("\r\x1b[0K\r\x1b[1A", end="")
+    page += 1
+    return (
+        "Location page {} {}".format(page, location.name),
+        [] if stop else [ex.submit(update_measurements, ex, location)]
+    )
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        update_countries()
+        with futures.ThreadPoolExecutor(max_workers=10) as ex:
+            tasks = set([ex.submit(update_countries, ex)])
+            while tasks:
+                done, tasks = futures.wait(
+                    tasks, return_when=futures.FIRST_COMPLETED)
+                for f in done:
+                    output, futs = f.result()
+                    tasks |= set(futs)
+                    print("{} {}".format(len(tasks), output))
